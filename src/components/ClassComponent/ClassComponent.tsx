677bb14d72e88e457bb8497696c95323d0a59ad2
Alex Holanda <alex.holanda@outlook.com>
2020-09-17 18:05:49 -0300

    ciclo de vida

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class ClassComponent extends React.Component<{ name: string }> {
+  
+  constructor(props: any) {
+    super(props);
+    console.log('Constructor reached');
+  }
+  
   state = {
     name: 'Mundo'
   }
 
+  componentDidMount() {
+    console.log('Component did mount reached');
+  }
+
+  componentDidUpdate() {
+    console.log('Component did update reached');
+  }
+
   render() {
+    console.log('Render reached');
+
     return <div>
       <p>Name: { this.state.name }</p>
       <button onClick={
