de616d5a8568b432bd15de176ed97f921c82f57b
Alex Holanda <alex.holanda@outlook.com>
2020-09-18 14:34:07 -0300

    remove useless code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,28 +4,13 @@ import './App.css';
 import Header from '../Header';
 import Container from '../../shared/Container';
 
-function TestComponent() {
-  return <img
-      width="16px"
-      src="https://www.iconfinder.com/data/icons/pixel-perfect-at-16px-volume-1/16/5023-512.png"
-      alt="search"/>
-}
-
 function App() {
 
   return (
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        <ul>
-          {
-            ['Daniel', 'William', 'Thiago', 'Daniel'].map((name, index) => {
-              return <li key={index}>
-                { name }
-              </li>
-            })
-          }
-        </ul>
+        
       </Container>
     </div>
   );
