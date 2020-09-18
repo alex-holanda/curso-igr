0240da75cd70e44a95ecbc4238df1d97b81c4269
Alex Holanda <alex.holanda@outlook.com>
2020-09-18 12:53:41 -0300

    add button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import './App.css';
 
 import Header from '../Header';
+import Button from '../Button';
+
+function TestComponent() {
+  return <img
+      width="16px"
+      src="https://www.iconfinder.com/data/icons/pixel-perfect-at-16px-volume-1/16/5023-512.png"
+      alt="search"/>
+}
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock" />
-      App
+      <div className="Container">
+        <Button 
+          onClick={ () => window.alert('Alerta') }
+          appendIcon={ <TestComponent /> }
+        >
+          Alert
+        </Button>
+      </div>
     </div>
   );
 }
