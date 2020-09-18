a7a580646b7de90e6ed2c272ab730725e8aff99a
Alex Holanda <alex.holanda@outlook.com>
2020-09-18 18:11:32 -0300

    table mock data

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,7 +11,8 @@ function App() {
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        <Table />
+        <Table 
+        />
       </Container>
     </div>
   );
