555178faa87584aea1af3c703ecd7d7249b6b418
Alex Holanda <alex.holanda@outlook.com>
2020-09-18 14:55:39 -0300

    add table html structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 
 import Header from '../Header';
 import Container from '../../shared/Container';
+import Table from '../../shared/Table';
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        
+        <Table />
       </Container>
     </div>
   );
--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -3,7 +3,21 @@ import './Table.scss';
 
 const Table = () => {
   return <table>
+    <thead>
+      <tr>
+        <th>Product</th>
+        <th>Price</th>
+        <th>Stock</th>
+      </tr>
+    </thead>
+      <tr>
+        <td>Cookie</td>
+        <td>$1.25</td>
+        <td>23</td>
+      </tr>
+    <tbody>
 
+    </tbody>
   </table>
 }
 
