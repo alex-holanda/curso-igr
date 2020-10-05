23ac7793eeb7f11eaa62cf762303b299a753c94f
Alex Holanda <alex.holanda@outlook.com>
2020-10-05 11:06:48 -0300

    add interceptor for Authorization

--- src/redux/Authentication/Authentication.reducer.ts
+++ src/redux/Authentication/Authentication.reducer.ts
@@ -3,13 +3,13 @@ import { Action } from '..';
 
 
 declare interface AuthenticationState {
-  profil?: User
+  profile?: User
 }
 
 export default function (state: AuthenticationState = {}, action: Action): AuthenticationState {
   switch (action.type) {
     case 'AUTHENTICATION_LOGIN':
-      return {profil: action.payload};
+      return { profile: action.payload };
 
     case 'AUTHENTICATION_LOGOUT':
       return {};
