a470becc3fe5789e7291e08b31c54e3cccfc3b8b
Alex Holanda <alex.holanda@outlook.com>
2020-10-06 21:01:40 -0300

    removed id from rendering

--- src/components/Products/ProductsCRUD.tsx
+++ src/components/Products/ProductsCRUD.tsx
@@ -10,7 +10,7 @@ import * as ProductsAction from '../../redux/Products/Products.actions';
 import { RootState, ThunkDispatch } from '../../redux';
 
 const headers: TableHeader[] = [
-  { key: 'id', value: '#' },
+  // { key: 'id', value: '#' },
   { key: 'name', value: 'Product' },
   { key: 'price', value: 'Price' },
   { key: 'stock', value: 'Available Stock', right: true }
